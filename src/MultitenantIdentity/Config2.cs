fd78b6deb2499947ad53369e9cf76515563ba6c4
Admir Durmishi <durmisi@gmail.com>
2018-11-18 13:19:04 +0100

    add identity server

---
+++ src/MultitenantIdentity/Config2.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace MultitenantIdentity
+{
+    public class Config2
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api2", "My API 2")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client2",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1_2" }
+                }
+            };
+        }
+    }
+}
