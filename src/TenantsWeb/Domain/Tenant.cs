eb5fe7670fb10e838c8dd6d6d02faeb22f34c1cf
Admir Durmishi <durmisi@gmail.com>
2018-12-07 15:28:19 +0100

    add Tenants web

---
+++ src/TenantsWeb/Domain/Tenant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TenantsWeb.Domain
+{
+    public class Tenant
+    {
+        public Tenant(Guid tenantGuid, string name)
+        {
+            TenantGuid = tenantGuid;
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid TenantGuid { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; internal set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ src/TenantsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TenantsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
